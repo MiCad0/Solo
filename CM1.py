c1c33556c3ba9ab10beb749d2892d8c8f0268a40
MiCad0 <denislinde4@gmail.com>
2025-10-02 12:44:29 +0200

    having fun with async functions (didn't work)

--- CM1.py
+++ CM1.py
@@ -1,10 +1,13 @@
+import asyncio
+import time
 import cv2
 import numpy
+import threading
 
 ERROSION = 0
-DILATATION = 1
+DILATION = 1
 
-def main():
+async def main():
     # Load an image
     image = cv2.imread('house.png')
     cv2.imshow('Image', image)
@@ -15,13 +18,16 @@ def main():
     cv2.imshow('Gray Image', gray_image)
     cv2.waitKey(0)
 
-
-    gradient_image = morphological_gradient(gray_image)
+    print(f"started at {time.strftime('%X')}")
+    gradient_image = await morphological_gradient(gray_image)
+    print(f"finished at {time.strftime('%X')}")
     cv2.imshow('Gradient Image', gradient_image)
+    # gradient_image2 = morphological_gradient(gradient_image)          # Less sharp image
+    # cv2.imshow('Gradient Image2', gradient_image2)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def errosion(image):
+async def errosion(image):
     rows, cols = image.shape
     erroded_image = image.copy()
     gamma4 = numpy.array([[0,0],[1,0],[-1,0],[0,1],[0,-1]])
@@ -33,7 +39,7 @@ def errosion(image):
             erroded_image[i][j] = min(n)
     return numpy.asarray(erroded_image)
 
-def dilation(image):
+async def dilation(image):
     rows, cols = image.shape
     erroded_image = image.copy()
     gamma4 = numpy.array([[0,0],[1,0],[-1,0],[0,1],[0,-1]])
@@ -41,23 +47,23 @@ def dilation(image):
 
     for i in range(rows):
         for j in range(cols):
-            n = visit_neighbors(image, [i,j], -gamma4, DILATATION)
+            n = visit_neighbors(image, [i,j], -gamma4, DILATION)
             erroded_image[i][j] = max(n)
     return numpy.asarray(erroded_image)
 
-def morphological_gradient(image):
-    erroded = errosion(image)
-    dilated = dilation(image)
-    return dilated - erroded
+async def morphological_gradient(image):
+    erroded = asyncio.create_task(errosion(image))
+    dilated = asyncio.create_task(dilation(image))
+    return await dilated - await erroded
 
 def visit_neighbors(image, origin, neighbors, default):
     res = set()
     for n in neighbors:
         i,j = [a + b for a, b in zip(origin, n)]
-        if (i<0 or j<0 or i>=image.shape[0] or j>=image.shape[1]): res.add(default==DILATATION and 255 or 0 )
+        if (i<0 or j<0 or i>=image.shape[0] or j>=image.shape[1]): res.add(default==DILATION and 255 or 0 )
         else: res.add(image[i][j])
     return res
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())

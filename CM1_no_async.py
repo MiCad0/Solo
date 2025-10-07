7977d511a7f6f9961c18fd6e7674b6ab9949ddb2
MiCad0 <denislinde4@gmail.com>
2025-10-07 17:26:41 +0200

    added opening closure, top/bottomhat algorithms and threshold

--- CM1_no_async.py
+++ CM1_no_async.py
@@ -1,4 +1,3 @@
-import asyncio
 import time
 import cv2
 import numpy
@@ -8,7 +7,7 @@ DILATION = 1
 GAMMA4 = numpy.array([[0,0],[1,0],[-1,0],[0,1],[0,-1]])
 GAMMA8 = numpy.array([[0,0],[1,0],[-1,0],[0,1],[0,-1],[1,1],[-1,-1],[1,-1],[-1,1]])
 
-async def main():
+def main():
     # Load an image
     image = cv2.imread('house.png')
     cv2.imshow('Image', image)
@@ -20,7 +19,7 @@ async def main():
     cv2.waitKey(0)
 
     print(f"started at {time.strftime('%X')}")
-    gradient_image = await morphological_gradient(gray_image)
+    gradient_image = morphological_gradient(gray_image)
     print(f"finished at {time.strftime('%X')}")
     cv2.imshow('Gradient Image', gradient_image)
     # gradient_image2 = morphological_gradient(gradient_image)          # Less sharp image
@@ -28,39 +27,43 @@ async def main():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-async def erosion(image,neighbors):
-    rows, cols = image.shape
-    erroded_image = image.copy()
+def erosion(image,neighbors):
+    print("starting erosion")
+    (rows, cols) = image.shape
+    eroded_image = numpy.empty_like(image)
 
     for i in range(rows):
         for j in range(cols):
             n = visit_neighbors(image, [i,j], neighbors, ERROSION)
-            erroded_image[i][j] = min(n)
-    return numpy.asarray(erroded_image)
+            eroded_image[i][j] = min(n)
+    print("ending erosion")
+    return eroded_image
 
-async def dilation(image,neighbors):
+def dilation(image,neighbors):
+    print("starting dilation")
     rows, cols = image.shape
-    erroded_image = image.copy()
+    dilated_image = numpy.empty_like(image)
 
     for i in range(rows):
         for j in range(cols):
             n = visit_neighbors(image, [i,j], -neighbors, DILATION)
-            erroded_image[i][j] = max(n)
-    return numpy.asarray(erroded_image)
+            dilated_image[i][j] = max(n)
+    print("ending dilation")
+    return dilated_image
 
-async def morphological_gradient(image):
-    erroded = asyncio.create_task(erosion(image,GAMMA4))
-    dilated = asyncio.create_task(dilation(image,GAMMA4))
-    return await dilated - await erroded
+def morphological_gradient(image):
+    erroded = erosion(image,GAMMA4)
+    dilated = dilation(image,GAMMA4)
+    return dilated - erroded
 
 def visit_neighbors(image, origin, neighbors, default):
-    res = set()
+    res = []
     for n in neighbors:
         i,j = [a + b for a, b in zip(origin, n)]
-        if (i<0 or j<0 or i>=image.shape[0] or j>=image.shape[1]): res.add(default==DILATION)
-        else: res.add(image[i][j])
+        if (i<0 or j<0 or i>=image.shape[0] or j>=image.shape[1]): res.append(default==DILATION)
+        elif image[i][j] not in res: res.append(image[i][j])
     return res
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
--- CM2.py
+++ CM2.py
@@ -1,59 +1,49 @@
-import asyncio
-import time
-import cv2
-import numpy
-
-ERROSION = 0
-DILATION = 1
-GAMMA4 = numpy.array([[0,0],[1,0],[-1,0],[0,1],[0,-1]])
-GAMMA8 = numpy.array([[0,0],[1,0],[-1,0],[0,1],[0,-1],[1,1],[-1,-1],[1,-1],[-1,1]])
+from CM1_no_async import *
 
 def main():
     # Load an image
-    image = cv2.imread('house.png')
-    cv2.imshow('Image', image)
-    cv2.waitKey(0)
-
-    # Turn the image to gray scale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Gray Image', gray_image)
-    cv2.waitKey(0)
-
+    image = cv2.imread('rice.png')
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+    # cv2.imshow('Image', gray_image)
+    # cv2.waitKey(0)
+    # print(B(2))
     print(f"started at {time.strftime('%X')}")
-    gradient_image = morphological_gradient(gray_image)
+    botHat = bottom_hat(gray_image,numpy.array(list(B(10))))
     print(f"finished at {time.strftime('%X')}")
-    cv2.imshow('Gradient Image', gradient_image)
-    # gradient_image2 = morphological_gradient(gradient_image)          # Less sharp image
-    # cv2.imshow('Gradient Image2', gradient_image2)
+    cv2.imwrite('output.png', botHat)
+    # botHat = cv2.imread('rice.png')
+    # botHat = cv2.cvtColor(botHat, cv2.COLOR_BGR2GRAY) 
+    cv2.imshow('rice', threshold(botHat,240))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def get_line(coords, index):
-    rows = []
-    for e in coords:
-        elem = filter(lambda a: a[index] == e[index], coords)
-        if elem not in rows: rows.append(elem)
-    return e
+def opening(image, neighbors):
+    print("Starting opening")
+    return dilation(erosion(image,neighbors),neighbors)
 
-def morphological_gradient(image):
-    # To parrallelize we need to get a list of coords for each row
-    # rows = get_line(GAMMA4,0)
-    # cols = get_line(GAMMA4,1)
-    eroded, dilated = numpy.zeros(image.shape),numpy.zeros(image.shape)
-    print(eroded.shape)
+def closing(image, neighbors):
+    print("Starting closing")
+    return erosion(dilation(image, neighbors),neighbors)
+
+def top_hat(image,neighbors):
+    return image - opening(image,neighbors)
+
+def bottom_hat(image,neighbors):
+    return closing(image,neighbors) - image
+
+def B(n):
+    x, y = numpy.mgrid[-n+1:n, -n+1:n]
+    return numpy.stack((x.ravel(), y.ravel()), axis=-1)
+
+def threshold(image,thresh):
+    print(image)
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
-            eroded[i][j] = min(visit_neighbors(image, [i,j], GAMMA4, ERROSION))
-            dilated[i][j] = max(visit_neighbors(image, [i,j], -GAMMA4, DILATION))
-    return dilated-eroded
-
-def visit_neighbors(image, origin, neighbors, default):
-    res = set()
-    for n in neighbors:
-        i,j = [a + b for a, b in zip(origin, n)]
-        if (i<0 or j<0 or i>=image.shape[0] or j>=image.shape[1]): res.add(default==DILATION and 255 or 0 )
-        else: res.add(image[i][j])
-    return res
+            if (image[i][j] > thresh): 
+                image[i][j] = 255 
+            else:
+                image[i][j] = 0
+    return image
 
 if __name__ == "__main__":
     main()
